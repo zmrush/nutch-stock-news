26c261eaf8a56a23cd4ac13063019f164723998e
edgar108 <edgar108@126.com>
2016-10-26 16:49:18 +0800

    qqtech

---
+++ src/plugin/parse-qqtech/src/java/org/apache/nutch/parse/qqtech/QQTechFilter.java
@@ -0,0 +1,227 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nutch.parse.qqtech;
+
+import org.apache.avro.util.Utf8;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.nutch.indexer.IndexingException;
+import org.apache.nutch.indexer.IndexingFilter;
+import org.apache.nutch.indexer.NutchDocument;
+import org.apache.nutch.parse.HTMLMetaTags;
+import org.apache.nutch.parse.Parse;
+import org.apache.nutch.parse.ParseFilter;
+import org.apache.nutch.parse.Parser;
+import org.apache.nutch.storage.WebPage;
+import org.apache.nutch.urlfilter.api.RegexRule;
+import org.apache.nutch.urlfilter.api.RegexURLFilterBase;
+import org.apache.nutch.util.NodeWalker;
+import org.apache.nutch.util.TableUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+//import org.w3c.dom.DocumentFragment;
+//import org.w3c.dom.Node;
+import org.w3c.dom.*;
+
+import java.io.IOException;
+import java.io.Reader;
+import java.lang.CharSequence;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Map.Entry;
+import java.util.regex.Pattern;
+import java.util.regex.PatternSyntaxException;
+
+/**
+ * Indexing filter that offers an option to either index all inbound anchor text
+ * for a document or deduplicate anchors. Deduplication does have it's con's,
+ * 
+ * @see {@code anchorIndexingFilter.deduplicate} in nutch-default.xml.
+ */
+public class QQTechFilter extends RegexURLFilterBase implements ParseFilter {
+  public static final Logger LOG = LoggerFactory
+      .getLogger(QQTechFilter.class);
+  private Configuration conf;
+
+  private static final Collection<WebPage.Field> FIELDS = new HashSet<WebPage.Field>();
+
+  static {
+    //FIELDS.add(WebPage.Field.INLINKS);
+  }
+//-------------------------------------------------------------------------------------
+public QQTechFilter() {
+  super();
+}
+
+  public QQTechFilter(String filename) throws IOException,
+          PatternSyntaxException {
+    super(filename);
+  }
+
+  QQTechFilter(Reader reader) throws IOException, IllegalArgumentException {
+    super(reader);
+  }
+  @Override
+  protected RegexRule createRule(boolean sign, String regex) {
+    return new Rule(sign, regex);
+  }
+
+  @Override
+  protected Reader getRulesReader(Configuration conf) throws IOException {
+    String fileRules = conf.get(QQTECH_REGEX_FILE);
+    return conf.getConfResourceAsReader(fileRules);
+  }
+  private class Rule extends RegexRule {
+
+    private Pattern pattern;
+
+    Rule(boolean sign, String regex) {
+      super(sign, regex);
+      pattern = Pattern.compile(regex);
+    }
+
+    protected boolean match(String url) {
+      return pattern.matcher(url).find();
+    }
+  }
+  public static final String QQTECH_REGEX_FILE = "qqtechindexingfilter.regex.file";
+  //----------------------------------------------------------------------------------
+
+  /**
+   * Set the {@link Configuration} object
+   */
+  //RegexURLFilterBase已经帮我们实现了这个玩意
+//  public void setConf(Configuration conf) {
+//    this.conf = conf;
+//  }
+
+  /**
+   * Get the {@link Configuration} object
+   */
+  public Configuration getConf() {
+    return this.conf;
+  }
+
+  public void addIndexBackendOptions(Configuration conf) {
+  }
+
+  /**
+   * The {@link AnchorIndexingFilter} filter object which supports boolean
+   * configuration settings for the deduplication of anchors. See
+   * {@code anchorIndexingFilter.deduplicate} in nutch-default.xml.
+   * 
+   * @param doc
+   *          The {@link NutchDocument} object
+   * @param url
+   *          URL to be filtered for anchor text
+   * @param page
+   *          {@link WebPage} object relative to the URL
+   * @return filtered NutchDocument
+   */
+  @Override
+  public Parse filter(String url, WebPage page, Parse parse,
+                      HTMLMetaTags metaTags, DocumentFragment doc){
+       LOG.info("urlparsing filter:"+url);
+       Parse newParse=null;
+       if(this.filter(url)!=null){
+         Combine combine=walk(doc);
+         newParse=new Parse(combine.text,parse.getTitle(),parse.getOutlinks(),parse.getParseStatus());
+         page.getHeaders().put(new Utf8("EditTime"),new Utf8(combine.time));
+         page.getHeaders().put(new Utf8("Source"),new Utf8("Seeking Alpha"));
+         return newParse;
+       }else{
+         newParse=new Parse(parse.getText(),parse.getTitle(),parse.getOutlinks(),parse.getParseStatus());
+       }
+    return newParse;
+  }
+  private class Combine{
+    public String text="";
+    public String time="";
+  }
+      private Combine walk(DocumentFragment root){
+        Combine combine=new Combine();
+        NodeWalker walker=new NodeWalker(root);
+        while(walker.hasNext()){
+          Node currentNode = walker.nextNode();
+          String nodeName = currentNode.getNodeName();
+          short nodeType = currentNode.getNodeType();
+          if("head".equalsIgnoreCase(nodeName))
+            walker.skipChildren();
+          if ("script".equalsIgnoreCase(nodeName)) {
+            walker.skipChildren();
+          }
+          if ("style".equalsIgnoreCase(nodeName)) {
+            walker.skipChildren();
+          }
+
+          if("span".equalsIgnoreCase(nodeName) && currentNode.getAttributes().getNamedItem("class") !=null
+                  && currentNode.getAttributes().getNamedItem("class").getNodeValue().equalsIgnoreCase("pubTime")){
+            combine.time=currentNode.getTextContent();
+          }
+
+          if("p".equalsIgnoreCase(nodeName) && currentNode.getAttributes().getNamedItem("style") !=null
+                  && currentNode.getAttributes().getNamedItem("style")
+                  .getNodeValue().equalsIgnoreCase("TEXT-INDENT: 2em")){
+
+            NodeList nodeList=currentNode.getChildNodes();
+//            System.out.println(nodeList.getLength());
+//            System.out.println(currentNode.getTextContent());
+//
+//            for(int j=0;j<nodeList.getLength();j++){
+//              System.out.println("child name:"+nodeList.item(j).getNodeName());
+//
+//            }
+            if(nodeList.getLength()==1 && nodeList.item(0).getNodeName().equalsIgnoreCase("#text")){
+              combine.text += currentNode.getTextContent()+"\r\n";
+
+            }
+
+            //  break;
+          }
+
+        }
+//        System.out.println("combine.text :"+combine.text );
+//        System.out.println("combine.time :"+combine.time );
+
+        return combine;
+
+  }
+//  @Override
+//  public NutchDocument filter(NutchDocument doc, String url, WebPage page)
+//      throws IndexingException {
+//    LOG.info("urlindexingfilter:"+url);
+//    if(this.filter(url)!=null) {  //接受url
+//      LOG.info("accept url");
+//      return doc;
+//
+//    }
+//    else {                       //否则拒绝这个url
+//      LOG.info("refuse url");
+//      return null;
+//    }
+//  }
+
+  /**
+   * Gets all the fields for a given {@link WebPage} Many datastores need to
+   * setup the mapreduce job by specifying the fields needed. All extensions
+   * that work on WebPage are able to specify what fields they need.
+   */
+  @Override
+  public Collection<WebPage.Field> getFields() {
+    return FIELDS;
+  }
+
+}
